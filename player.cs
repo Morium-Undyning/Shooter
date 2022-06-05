c1e6046c341a848423d7eb898139507db49c6a91
Morium-Undyning <106692310+Morium-Undyning@users.noreply.github.com>
2022-06-06 01:15:43 +0700

    Update player.cs

--- player.cs
+++ player.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int heal;
+    private float speed;
+
+    private Animator anim;
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+
+
     void Start()
     {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         int i2;
     }
 
@@ -14,5 +23,25 @@ public class player : MonoBehaviour
     void Update()
     {
         
+    }
+    private void FixedUpdate()
+    {
+
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+        if (facingRight == false)
+        {
+            Flip();
+        }
+        else if (facingRight == true)
+        {
+            Flip();
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 }

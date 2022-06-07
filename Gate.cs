0ce430e4927c5638952323a4ba99ef854009c312
Morium-Undyning <106692310+Morium-Undyning@users.noreply.github.com>
2022-06-07 08:59:45 +0700

    Gate-1

--- Gate.cs
+++ Gate.cs
@@ -3,24 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class Gate : MonoBehaviour
 {
+    int k;
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene("Scene2");
+            if (k==1) {
+                k++;
+                SceneManager.LoadScene("Scene2");
+            }
+            if (k == 2)
+            {
+                k++;
+                SceneManager.LoadScene("Scene3");
+            }
+            if (k == 3)
+            {
+                k++;
+                SceneManager.LoadScene("Boss");
+            }
         }
     }
 }
